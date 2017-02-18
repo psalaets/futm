c737abc636d74c825fb012fb97072a4e34c6ece9
Paul Salaets <psalaets@gmail.com>
2017-02-18 12:26:37 -0500

    more code to deal with pathname than the actual utm stuff but ok

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 module.exports = forgetUtm;
 
 function forgetUtm(url) {
-  return url;
+  const modified = new URL(url);
+  const params = modified.searchParams;
+
+  for (let key of params.keys()) {
+    if (/^utm_/.test(key)) {
+      params.delete(key);
+    }
+  }
+
+  return preservePathname(url, modified);
+}
+
+// all this to prevent https://blah.com => https://blah.com/
+function preservePathname(source, modified) {
+  const parts = new URL(source);
+
+  // path is empty or a single slash
+  if (parts.pathname === '/') {
+    // it really is a single slash
+    if (source[parts.origin.length] === '/') {
+      return modified.toString();
+    } else {
+      return modified.origin + trimTrailingSlash(modified.pathname) + modified.search + modified.hash;
+    }
+  } else {
+    return modified.toString();
+  }
+}
+
+function trimTrailingSlash(str) {
+  return str.endsWith('/') ? str.slice(0, -1) : str;
 }
--- test.js
+++ test.js
@@ -9,6 +9,13 @@ test('makes no change if no query params', t => {
   t.equals(result, 'https://blah.com');
 });
 
+test('preserves trailing slash', t => {
+  t.plan(1);
+
+  var result = forgetUtm('https://blah.com/');
+
+  t.equals(result, 'https://blah.com/');
+});
 
 test('makes no change if no utm query params', t => {
   t.plan(1);
@@ -16,4 +23,28 @@ test('makes no change if no utm query params', t => {
   var result = forgetUtm('https://blah.com?name=ok&count=3');
 
   t.equals(result, 'https://blah.com?name=ok&count=3');
+});
+
+test('removes utm query param', t => {
+  t.plan(1);
+
+  var result = forgetUtm('https://blah.com?utm_thing=twitter');
+
+  t.equals(result, 'https://blah.com');
+});
+
+test('removes only utm query params', t => {
+  t.plan(1);
+
+  var result = forgetUtm('https://blah.com?age=5&utm_thing=twitter&name=ok&utm_other=yes');
+
+  t.equals(result, 'https://blah.com?age=5&name=ok');
+});
+
+test('removal from busy url', t => {
+  t.plan(1);
+
+  var result = forgetUtm('https://blah.com:80/a/b/c?age=5&utm_thing=twitter#top');
+
+  t.equals(result, 'https://blah.com:80/a/b/c?age=5#top');
 });
